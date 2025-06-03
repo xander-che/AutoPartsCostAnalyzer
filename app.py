fd5c62198895e7061ca2f05915ee4008f7279c42
xander-che <achernys123@gmail.com>
2025-06-03 19:06:12 +0300

    formatted

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask
 from controllers.entry_params_controller import params
 
-
 app = Flask(__name__)
 
 app.register_blueprint(params)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
