36b0a0581c07f8c1c82b3298f11311ab62291fad
F1rst321 <ilya.nvr@outlook.com>
2025-06-05 19:43:30 +0300

    params dict

--- services/params_dict.py
+++ services/params_dict.py
@@ -5,9 +5,7 @@ def get_params_dict(rating: float,
                     availability: int,
                     delivery_time: int,
                     data_source: str) -> EntryParams:
-    return EntryParams(
-        rating=rating,
-        availability=availability,
-        delivery_time=delivery_time,
-        data_source=data_source
-    )
+    return EntryParams(rating=rating,
+                       availability=availability,
+                       delivery_time=delivery_time,
+                       data_source=data_source)
