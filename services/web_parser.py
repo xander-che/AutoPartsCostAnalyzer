8a552629e755cfc1e34a4fc9132a4ea82bd79c34
xander-che <achernys123@gmail.com>
2025-06-14 19:53:53 +0300

    parser update

--- services/web_parser.py
+++ services/web_parser.py
@@ -48,7 +48,7 @@ class EMEXParser:
 
                 raw_data = json.loads(soup.find(id='__NEXT_DATA__').prettify()[51:-10])
                 result_table.append(self.__parse_data(raw_data, item[1]))
-                sleep(2)
+                sleep(1)
 
         except Exception as e:
             print(f"Ошибка соединения: {e}")
